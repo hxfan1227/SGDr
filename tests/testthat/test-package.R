b6a5d8d864e9a234c597f70c84090bf80268cff6
Hongxiang Fan <hxfan@niglas.ac.cn>
2024-03-25 00:01:05 +1030

    update test

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -4,14 +4,14 @@
 # 
 # 
 # const_params <- list(
-#   x = 3253, 
+#   x = 3253,
 #   W = 5864,
 #   Area = 36885913,
 #   L = 6760,
-#   waterContent = list(Texture = c("Sand", "Loam", "Clay"), 
-#                       ClayContent = c(3, 22, 47), 
-#                       Saturation = c(0.4, 0.5, 0.6), 
-#                       FC = c(0.06, 0.29, 0.41), 
+#   waterContent = list(Texture = c("Sand", "Loam", "Clay"),
+#                       ClayContent = c(3, 22, 47),
+#                       Saturation = c(0.4, 0.5, 0.6),
+#                       FC = c(0.06, 0.29, 0.41),
 #                       PWP = c(0.02, 0.05, 0.2)
 #   )
 # )
@@ -24,7 +24,7 @@
 # 
 # sgd_mod <- create_sgd_model(test_data, tanya_params, const_params)
 # sgd_mod$calc_recharge()
-# sgd_mod$calc_sgd(30)
+# sgd_mod$calc_sgd(180)
 # plot(sgd_mod$get_H2O3_AQ(), ylim = c(0, 2))
 # sgd_mod$update_parameters(preferred_params, const_params)
 # sgd_mod$calc_recharge()
@@ -32,5 +32,4 @@
 # lines(sgd_mod$get_H2O3_AQ(), col = 'red')
 # 
 # plot(sgd_mod$get_xn(), col = 'blue')
-# 
 # 
