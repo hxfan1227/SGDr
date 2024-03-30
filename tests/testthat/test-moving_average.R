3434d4765539c83ad11e49e31edb0c9ac0807c21
Hongxiang Fan <hxfan@niglas.ac.cn>
2024-03-30 17:55:21 +1030

    remove test

--- tests/testthat/test-moving_average.R
+++ tests/testthat/test-moving_average.R
@@ -1,6 +1,7 @@
-test_that("moving_average works", {
-    expect_equal(
-        moving_average(1:5, 3),
-        c(2, 2, 2, 3, 4)
-    )
-})
+# test_that("moving_average works", {
+#     expect_equal(
+#         moving_average(1:5, 3),
+#         c(2, 2, 2, 3, 4)
+#     )
+# })
+# 
