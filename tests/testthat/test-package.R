2a1418241e734a2847fa7d55ae759b11fdaea523
Hongxiang Fan <hxfan@niglas.ac.cn>
2024-04-15 10:32:10 +0930

    add testdata

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -1,47 +1,72 @@
-# # Load the preferred parameters 
-# preferred_params_list <- json_to_paramter_list('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/json/preferred.json')
-# # Load Tanya's calibrated parameters
-# tanya_params_list <- json_to_paramter_list('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/json/tanya.json')
-# # Load the constant parameters
-# const_params_list <- json_to_paramter_list('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/json/consts.json')
-# # Load the lower bounds of the parameters
-# lower_bounds_list <- json_to_paramter_list('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/json/lower_limits.json')
-# # Load the upper bounds of the parameters
-# upper_bounds_list <- json_to_paramter_list('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/json/upper_limits.json')
-# # Load the maximum bounds of the parameters
-# max_bounds_list <- json_to_paramter_list('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/json/maximum.json')
-# # Load the minimum bounds of the parameters
-# min_bounds_list <- json_to_paramter_list('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/json/minimum.json')
-# # Load the skeleton of the parameters
-# params_skeleton <- json_to_paramter_list('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/json/preferred.json')
-# # Load the observed data
-# obs_data <- read_csv('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/obs_data.csv') %>% 
-#   mutate(date = dmy(obs_date)) %>% 
-#   select(date, wl) %>% 
-#   distinct(date, .keep_all = T) %>% 
-#   filter(wl <= 2) # to eliminate the outliers
-# # load the input data
-# test_data <- read_csv('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/test_data.csv')
-# ids <- as.integer(obs_data$date - ymd('1967-01-01') + 1)
+test_that("all input data have been coerced to correct type", {
+  expect_s3_class(json_to_paramter_list(test_path('testdata', 'preferred.json')), 'list')
+  expect_s3_class(json_to_paramter_list(test_path('testdata', 'consts.json')), 'list')
+})
 
-# obs_dt <- read_csv('/mnt/c/RProjects/DailyWork/CoffinBay/Scripts/SGD/Data/obs_data.csv') %>% 
-#   select(obs_date, wl) %>%
-#   filter(wl <= 2) %>%
-#   rename('date' = 'obs_date') %>%
-#   pivot_longer(-date, values_to = 'obs') %>%
-#   mutate(name = factor(name, levels = c('wl', 'SGD', ' hn', 'xn', 'Wrechg', 'WrechgAve'),
-#                        labels = c('WL~(m~AHD)',
-#                                   'SGD~(m^3/d)',
-#                                   'h[n]~(m~AHD)',
-#                                   'x[n]~(m)',
-#                                   'w[recharge]~(mm)',
-#                                   'w[recharge[ave]]~(mm)')),
-#          date = dmy(date))
+test_that("calibratable parameters are correctly loaded", {
+  x <- json_to_paramter_list(test_path('testdata', 'preferred.json'))
+  expect_contains(names(x), c('bucket1', 'bucket2', 'curveNumber', 'aquifer'))
+  expect_contains(names(x$bucket1), c('layer', 'z', 'rho_b', 'mc', 'rho_s', 'Ksat', 'n', 'Y'))
+  expect_contains(names(x$bucket2), c('layer', 'z', 'rho_b', 'mc', 'rho_s', 'Ksat', 'n', 'Z'))
+  expect_contains(names(x$curveNumber), c('CN2', 'PIa'))
+  expect_contains(names(x$aquifer), c('delta', 'rho_s', 'rho_f', 'K', 'z0', 'Sy', 'xT', 'dxT'))
+})
 
-# x1 <- estimate_sgd(test_data, tanya_params_list, const_params_list, 30)
-# x2 <- estimate_sgd_fixed_hxn(test_data, tanya_params_list, const_params_list, 30, xn = 7000, hn = 28)
-# plot(x1$wl, type = 'l', lty = 2)
-# lines(x2$wl, col = 'red')
-# legend('topright', c("Without fixed hn and xn", "With fixed hn and xn"),
-#        col = c('black', 'red'), lty = 2)
-# model_diagnose(x2, obs_dt, '1967-01-01')
+test_that("constant parameters are correctly loaded", {
+  x <- json_to_paramter_list(test_path('testdata', 'consts.json'))
+  expect_contains(names(x), c('x', 'W', 'Area', 'L', 'waterContent'))
+  expect_type(x$waterContent, 'list')
+})
+
+test_that("estimate_sgd() checks parameters", {
+  expect_error(estimate_sgd(1:2, 1:2, 1:2, 120, 1500))
+})
+
+test_that("estimate_sgd() returns correct type", {
+  inputData <- read.csv(test_path('testdata', 'test_data.csv'))
+  calibratableParamsList <- json_to_paramter_list(test_path('testdata', 'preferred.json'))
+  constParamsList <- json_to_paramter_list(test_path('testdata', 'consts.json'))
+  expect_s3_class(estimate_sgd(inputData, calibratableParamsList, constParamsList), 'SGD_ESTIMATION_DF')
+})
+
+test_that("print() warns if no base_date is available", {
+  inputData <- read.csv(test_path('testdata', 'test_data.csv'))
+  calibratableParamsList <- json_to_paramter_list(test_path('testdata', 'preferred.json'))
+  constParamsList <- json_to_paramter_list(test_path('testdata', 'consts.json'))
+  x <- estimate_sgd(inputData, calibratableParamsList, constParamsList)
+  expect_warning(print(x), 'No base date available. Please provide the base date to calculate the simulation period. \n')
+  expect_warning(summary(x), 'No base date available. Please provide the base date to calculate the simulation period. \n')
+})
+
+test_that("plot() returns correct object type", {
+  inputData <- read.csv(test_path('testdata', 'test_data.csv'))
+  obsData <- read.csv(test_path('testdata', 'obs_data.csv')) %>% 
+    mutate(date = ymd(obs_date)) %>%
+      select(date, wl) %>%
+      distinct(date, .keep_all = T) %>%
+      filter(wl <= 2)
+  calibratableParamsList <- json_to_paramter_list(test_path('testdata', 'preferred.json'))
+  constParamsList <- json_to_paramter_list(test_path('testdata', 'consts.json'))
+  x <- estimate_sgd(inputData, calibratableParamsList, constParamsList)
+  y <- plot(x, y = obsData, type = 'comp', base_date = 19670101)
+  expect_s3_class(y, 'SGD_ESTIMATION_DF')
+  expect_equal(x, y)
+})
+
+test_that("plot() throws error when no base_date is available", {
+  inputData <- read.csv(test_path('testdata', 'test_data.csv'))
+  obsData <- read.csv(test_path('testdata', 'obs_data.csv')) %>% 
+    mutate(date = ymd(obs_date)) %>%
+      select(date, wl) %>%
+      distinct(date, .keep_all = T) %>%
+      filter(wl <= 2)
+  calibratableParamsList <- json_to_paramter_list(test_path('testdata', 'preferred.json'))
+  constParamsList <- json_to_paramter_list(test_path('testdata', 'consts.json'))
+  x <- estimate_sgd(inputData, calibratableParamsList, constParamsList)
+  expect_error(plot(x, y = obsData, type = 'comp'))
+  expect_error(plot(x, y = obsData, type = 'input'))
+  expect_error(plot(x, y = obsData, type = 'pred'))
+})
+
+
+
